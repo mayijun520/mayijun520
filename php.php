dc14d3a273b2318697f61c299fd65631bfc61508
mayijun520 <92634947+mayijun520@users.noreply.github.com>
2021-10-16 21:44:40 +0800

    Create php.php

---
+++ php.php
@@ -0,0 +1,18 @@
+<?php
+$urls = array(
+    
+    'https://mayijun520.github.io/',
+);
+$api = 'http://data.zz.baidu.com/urls?site=https://mayijun520.github.io&token=UbtGHKLtDjsnBUCy';
+$ch = curl_init();
+$options =  array(
+    CURLOPT_URL => $api,
+    CURLOPT_POST => true,
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_POSTFIELDS => implode("\n", $urls),
+    CURLOPT_HTTPHEADER => array('Content-Type: text/plain'),
+);
+curl_setopt_array($ch, $options);
+$result = curl_exec($ch);
+echo $result;
+?>
