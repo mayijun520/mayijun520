58d596d4d419def958ff3e5e50561cbf6186781d
mayijun520 <92634947+mayijun520@users.noreply.github.com>
2021-10-20 15:33:36 +0800

    Update conn.php

--- php/conn.php
+++ php/conn.php
@@ -1,5 +1,5 @@
 <?php
-$conn = new mysqli("localhost","s0515630","LDY2000916","love_sentence");//数据库信息
+$conn = new mysqli("localhost","","6","love_sentence");//数据库信息
 if($conn->connect_error){
     die("数据连接失败");
 }
